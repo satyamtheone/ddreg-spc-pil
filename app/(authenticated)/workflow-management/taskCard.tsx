b740d3cabd8e1ec7f65c17a8814511c0980085a4
satyam <satyam.s@ddreg.in>
2026-07-17 16:56:02 +0530

    admin delete added

--- app/(authenticated)/workflow-management/taskCard.tsx
+++ app/(authenticated)/workflow-management/taskCard.tsx
@@ -211,44 +211,44 @@ const TaskCard: React.FC<TaskCardProps> = ({ task, params }) => {
               />
             </div>
           )}
-          {(canDoAction || user?.id === task.createdById) &&
-            (task.status === "CREATED" || task.status === "UNDER_EDITING") && (
-              <div className=" flex items-center gap-2">
-                {task.status === "CREATED" && (
-                  <DynamicButton
-                    icon={<FaEye className="text-teal-600" size={25} />}
-                    size="slim"
-                    className="px-2"
-                    variant="card"
-                    onClick={() =>
-                      goTo(
-                        `/document-editor/fullpageEditor?documentBufferUrl=${task?.documentVersion?.documentVersionFile?.key || document?.data.reference.referenceFile.key}&type=${task?.documentVersion?.reference?.schemaMeta?.type}&region=${task?.documentVersion?.reference?.type?.country?.code}&versionId=${task.documentVersion.id}&role=VIEWER`,
-                      )
-                    }
-                  />
-                )}
-
+          {(user?.id === task.createdById ||
+            (canDoAction &&
+              (task.status === "CREATED" ||
+                task.status === "UNDER_EDITING"))) && (
+            <div className=" flex items-center gap-2">
+              {task.status === "CREATED" && (
                 <DynamicButton
-                  icon={<HiOutlineTrash size={25} />}
+                  icon={<FaEye className="text-teal-600" size={25} />}
                   size="slim"
-                  variant="danger"
                   className="px-2"
-                  onClick={() => {
-                    openDialog({
-                      children: (
-                        <ModalProvider
-                          size="md:w-200 w-11/12 "
-                          title={`Delete Task`}
-                          children={
-                            <DeleteTaskDialog Id={task.id} Name="Task" />
-                          }
-                        />
-                      ),
-                    });
-                  }}
+                  variant="card"
+                  onClick={() =>
+                    goTo(
+                      `/document-editor/fullpageEditor?documentBufferUrl=${task?.documentVersion?.documentVersionFile?.key || document?.data.reference.referenceFile.key}&type=${task?.documentVersion?.reference?.schemaMeta?.type}&region=${task?.documentVersion?.reference?.type?.country?.code}&versionId=${task.documentVersion.id}&role=VIEWER`,
+                    )
+                  }
                 />
-              </div>
-            )}
+              )}
+
+              <DynamicButton
+                icon={<HiOutlineTrash size={25} />}
+                size="slim"
+                variant="danger"
+                className="px-2"
+                onClick={() => {
+                  openDialog({
+                    children: (
+                      <ModalProvider
+                        size="md:w-200 w-11/12 "
+                        title={`Delete Task`}
+                        children={<DeleteTaskDialog Id={task.id} Name="Task" />}
+                      />
+                    ),
+                  });
+                }}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
